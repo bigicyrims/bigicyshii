c7ef198e0a9d31583890622c0af9fb9724e73255
bigicyrims <beetle_length.1p@icloud.com>
2026-01-06 12:03:22 -0600

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,8 @@
+{
+  "videos": [
+    { "src": "media/videos/video1.mp4", "thumb": "media/thumbs/video1.jpg", "title": "Video 1" }
+  ],
+  "photos": [
+    { "src": "media/photos/photo1.jpg", "title": "Photo 1" }
+  ]
+}
